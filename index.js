3be157ecaf8b32d99234929fa70f33cc1dbb750e
kappnjusdev <kappnjus@gmail.com>
2026-05-05 18:58:26 -0400

    fix javascript issue
    The page javascript didn't function because
    the script tag was missing the type="module"
    attribute. This is required to use ES6 module
    syntax in the index.js file, which is necessary
    for the import statements to work correctly.
    By adding type="module" to the script tag,
    the browser can properly interpret the JavaScript
    code and execute it without errors.
    
    Added dist and node_modules after running
    the npm run build command.
    
    Added functions to the global window in index.js
    to make them accessible in the HTML file, allowing
    the buttons to call these functions when clicked.
    (ex. window.add1PointsToHome = add1PointsToHome;)

--- index.js
+++ index.js
@@ -39,3 +39,12 @@ function newGame() {
   homeScoreEl.textContent = homeScore;
   guestScoreEl.textContent = guestScore;
 }
+
+// Attach functions to window for global access
+window.add1PointsToHome = add1PointsToHome;
+window.add2PointsToHome = add2PointsToHome;
+window.add3PointsToHome = add3PointsToHome;
+window.add1PointsToGuest = add1PointsToGuest;
+window.add2PointsToGuest = add2PointsToGuest;
+window.add3PointsToGuest = add3PointsToGuest;
+window.newGame = newGame;
