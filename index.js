768e797208d45aae0fd06067f53e1dbe81dcaf23
kappnjusdev <kappnjus@gmail.com>
2026-05-05 18:49:36 -0400

    changed team font and code spacing

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-let homeScoreEl = document.getElementById("homeScore")
-let guestScoreEl = document.getElementById("guestScore")
-let homeScore = 0
-let guestScore = 0
+let homeScoreEl = document.getElementById("homeScore");
+let guestScoreEl = document.getElementById("guestScore");
+let homeScore = 0;
+let guestScore = 0;
 
 function add1PointsToHome() {
-    homeScore += 1
-    homeScoreEl.textContent = homeScore
+  homeScore += 1;
+  homeScoreEl.textContent = homeScore;
 }
 
 function add2PointsToHome() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
+  homeScore += 2;
+  homeScoreEl.textContent = homeScore;
 }
 
 function add3PointsToHome() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
+  homeScore += 3;
+  homeScoreEl.textContent = homeScore;
 }
 
 function add1PointsToGuest() {
-    guestScore += 1
-    guestScoreEl.textContent = guestScore
+  guestScore += 1;
+  guestScoreEl.textContent = guestScore;
 }
 
 function add2PointsToGuest() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
+  guestScore += 2;
+  guestScoreEl.textContent = guestScore;
 }
 
 function add3PointsToGuest() {
-    guestScore += 3
-    guestScoreEl.textContent = guestScore
+  guestScore += 3;
+  guestScoreEl.textContent = guestScore;
 }
 
 function newGame() {
-    homeScore = 0
-    guestScore = 0
-    homeScoreEl.textContent = homeScore
-    guestScoreEl.textContent = guestScore
+  homeScore = 0;
+  guestScore = 0;
+  homeScoreEl.textContent = homeScore;
+  guestScoreEl.textContent = guestScore;
 }
